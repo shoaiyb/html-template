48e3c65293a8e8049527b85e4987382522691043
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-01-16 02:11:16 +0100

    Create rss_redirect.js

---
+++ assets/js/rss_redirect.js
@@ -0,0 +1,21 @@
+<script language=’javascript’>
+
+// url shortener by shoaiyb sysa
+
+//<![CDATA[
+var key = window.location.href.split(“feed/”)[1].replace(“/”,””)
+var urls={
+‘rss’:’https://templass.ga/sitemap.xml’,
+‘’:’https://templass.ga/sitemap.xml’,
+// ‘’:”https://templass.ga/sitemap.xml”,
+// ‘’:”https://templass.ga/sitemap.xml”,
+}
+if(key){
+if(urls[key]){
+window.location.href=urls[key]
+}else{
+document.write(“‘”+key+”‘ not found :(“);
+}
+}
+//]]>
+</script>
