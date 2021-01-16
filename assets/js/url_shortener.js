4209830dee0f7c73f85339954c25b274ee1c1194
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-01-16 02:03:34 +0100

    Create url_shortener.js

---
+++ assets/js/url_shortener.js
@@ -0,0 +1,21 @@
+<script language=’javascript’>
+
+// url shortener by shoaiyb sysa
+
+//<![CDATA[
+var key = window.location.href.split(“go/”)[1].replace(“/”,””)
+var urls={
+‘fb’:’https://www.facebook.com/realshoaiyb’,
+‘ig’:’https://www.instagram.com/sysa_shoaiyb’,
+‘gh’:”https://github.com/shoaiyb”,
+‘cp’:”https://codepen.io/shoaiyb”,
+}
+if(key){
+if(urls[key]){
+window.location.href=urls[key]
+}else{
+document.write(“‘”+key+”‘ not found :(“);
+}
+}
+//]]>
+</script>
